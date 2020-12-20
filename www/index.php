9ee9f650e8b55b11a2d81b6774e309d6cf4d9f13
Germain SIPIERE <gsipiere@gmail.com>
2020-12-20 14:53:02 +0100

    readme modifié

--- www/index.php
+++ www/index.php
@@ -13,7 +13,7 @@
     <div class="m-5 py-12 shadow-inner rounded-md bg-white">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <figure class="md:flex rounded-xl p-8 md:p-0">
-                <img class="h-auto md:rounded-none mx-auto" src="assets/img/logo_afpa.png" alt="" width="350">
+                <img class="h-auto md:rounded-none mx-auto" src="assets/img/afpaLamp.png" alt="" width="350">
                 <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
                     <div class="lg:text-center">
                         <h2 class="text-base text-green-600 font-semibold tracking-wide uppercase">AFPA LAMP</h2>
@@ -96,8 +96,8 @@
                                 <div class="flex items-center justify-center h-12 w-12 rounded-md bg-green-400 text-white">
                                     <!-- Heroicon name: globe-alt -->
                                     <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                                    </svg>
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+</svg>
                                 </div>
                             </div>
                             <div class="ml-4 divide-y-4 divide-green-400 divide-solid">
@@ -157,6 +157,9 @@
         </div>
     </div>
 
+<footer class="text-center text-sm">
+    <div class="text-white">Réalisé par <a class="font-bold" href="">Peter <span class="text-yellow-600">6</span></a></div>
+</footer>
 
 </body>
 
